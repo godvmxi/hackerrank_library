686d7df3754ee3e75c8e539e855d4a78f3d110f2
jbright <bright.jiang@amd.com>
2021-09-26 14:07:57 +0800

    add append-and-delete

--- implementation/append-and-delete/main.cpp
+++ implementation/append-and-delete/main.cpp
@@ -1,50 +1,73 @@
 #include <bits/stdc++.h>
-#include <list>
 
 using namespace std;
 
 string ltrim(const string &);
 string rtrim(const string &);
-vector<string> split(const string &);
 
+/*
+ * Complete the 'appendAndDelete' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. STRING t
+ *  3. INTEGER k
+ */
 
+string appendAndDelete(string s, string t, int k) {
+    int len_s = s.length();
+    int len_t = t.length();
+    int short_len = len_s < len_t ? len_s : len_t;
+
+    int len_match = 0;
+
+    for(int i =0; i < short_len; i++)
+    {
+        //cout << "compare: " << i << "->" << s[i] << "<-  "<< "->" << t[i] << "<-  " << endl;
+        if(s[i] != t[i])
+        {
+            //len_match = i;
+            break;
+        }
+        else
+        {
+            len_match = i+1;
+        }
+    }
+    cout << "src: " << s << endl;
+    cout << "dst: " << t << endl;
+    cout << "src: " << len_s
+            << " dst: " << len_t
+            << " min: " << short_len
+            << " match: " << len_match
+            << " limit:"<< k <<endl;
+    int tot = (len_s - len_match) + (len_t -len_match);
+    //cout << "result: " << result << endl;
+    //return tot <= k ? "Yes": "No";
+    return (tot == k) || ( ( tot < k) && ( (tot - k)%2 ==0 ) || (tot + (2 * len_match) <= k) )  ? "Yes": "No";
+}
 
 int main()
 {
-    string N_temp;
-    getline(cin, N_temp);
+    ofstream fout(getenv("OUTPUT_PATH"));
 
-    int N = stoi(ltrim(rtrim(N_temp)));
-    vector<string> userList;
-    for (int N_itr = 0; N_itr < N; N_itr++) {
-        string first_multiple_input_temp;
-        getline(cin, first_multiple_input_temp);
+    string s;
+    getline(cin, s);
 
-        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+    string t;
+    getline(cin, t);
 
-        string firstName = first_multiple_input[0];
+    string k_temp;
+    getline(cin, k_temp);
 
-        string emailID = first_multiple_input[1];
-        string mailFix = "@gmail.com";
-        int mailFixLen = mailFix.length();
-        int emailIDLen = emailID.length();
-        if(emailIDLen > mailFixLen)
-        {
-            if(emailID.compare(emailIDLen - mailFixLen, mailFixLen, mailFix) == 0)
-            {
-                //cout << firstName << endl;
-                userList.push_back(firstName);
-            }
-        }
-    }
-    if(userList.size() > 0)
-    {
-        sort(userList.begin(),userList.end());
-    }
-    for(auto user : userList)
-    {
-        cout << user <<endl;
-    }
+    int k = stoi(ltrim(rtrim(k_temp)));
+
+    string result = appendAndDelete(s, t, k);
+
+    cout << result << "\n";
+
+    fout.close();
 
     return 0;
 }
@@ -69,21 +92,4 @@ string rtrim(const string &str) {
     );
 
     return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
